09263ba1a195fd22414dab44c42c249619d7927a
monjara <yaginumee@gmail.com>
2020-06-30 16:41:03 +0900

    コントローラの作成

---
+++ src/main/java/com/example/springrest/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.springrest.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+	
+	EmployeeNotFoundException(Long id) {
+		super("Could not find employee" + id);
+	}
+}
