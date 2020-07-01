579b6ef75005afc58b2e16b025e7156a8cfa0df4
monjara <yaginumee@gmail.com>
2020-07-02 04:14:34 +0900

    はじめてのコミット

---
+++ src/main/java/reactspringdatarest/ReactSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package reactspringdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringDataRestApplication.class, args);
+	}
+
+}
