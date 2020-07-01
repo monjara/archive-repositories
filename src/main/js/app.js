19a935091bdd62bbd5ebaf8e4b5ca59bace062c8
monjara <yaginumee@gmail.com>
2020-07-02 05:21:01 +0900

    EmployeeListコンポーネントの作成

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -21,3 +21,23 @@ class App extends React.Component {
         )
     }
 }
+
+class EmployeeList extends React.Component {
+    render() {
+        const employees = this.props.employees.map(employees =>
+            <Employee key={employee._links.self.href} employee={employee}/>
+        );
+
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Description</th>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
