034c40304d3a8c86c0d734995e513dd12b74ee04
monjara <yaginumee@gmail.com>
2020-07-02 16:06:45 +0900

    アノテーションを追加

--- src/main/java/todo/entity/Folder.java
+++ src/main/java/todo/entity/Folder.java
@@ -1,7 +1,9 @@
 package todo.entity;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -21,17 +23,22 @@ public class Folder {
     
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     private String title;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
     private Date createdAt;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
     private Date updatedAt;
 
-    @OneToMany
-    private Task task;
+    // https://gist.github.com/momotar/edccbea0e9712a3b3a6e　を参照した
+    @OneToMany(mappedBy = "folder")
+    private List<Task> tasks;
 
 }
--- src/main/java/todo/entity/Task.java
+++ src/main/java/todo/entity/Task.java
@@ -2,9 +2,12 @@ package todo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -19,24 +22,35 @@ public class Task {
     
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private int id;
 
+    // この変数のカラム名がFolder folderとかぶっているがどうなるかわからないため
+    // とりあえず残しておく
+    @Column(name = "folder_id")
     private int folderId;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "status")
     private int status;
 
+    @Column(name = "due_date")
     @Temporal(TemporalType.DATE)
     private Date dueDate;
 
+    @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
+    @Column(name = "updated_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt;
 
-    @ManyToOne
+    // https://gist.github.com/momotar/edccbea0e9712a3b3a6e　を参照した
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id", referencedColumnName = "id")
     private Folder folder;
 
 }
