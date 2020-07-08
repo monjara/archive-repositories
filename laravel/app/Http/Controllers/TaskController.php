e409a1af6628a9c37c84b2bc52f0b1281b1ebce2
monjara <yaginumee@gmail.com>
2020-07-08 17:40:55 +0900

    タスクを取得してviewに渡す

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -43,4 +43,13 @@ class TaskController extends Controller
         ]);
     }
 
+    public function showEditForm(int $id, int $task_id)
+{
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+}
+
 }
