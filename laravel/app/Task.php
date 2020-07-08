c67470014db28e8df95bde53aee5f0d934033db5
monjara <yaginumee@gmail.com>
2020-07-08 14:32:08 +0900

    タスクの状態の色分け

--- laravel/app/Task.php
+++ laravel/app/Task.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     const STATUS = [
-        1 => [ 'label' => '未着手'],
-        2 => [ 'label' => '着手中'],
-        3 => [ 'label' => '完了'],
+        1 => [ 'label' => '未着手', 'class' => 'label-danger'],
+        2 => [ 'label' => '着手中', 'class' => 'label-info'],
+        3 => [ 'label' => '完了', 'class' => ''],
     ];
 
     public function getStatusLabelAttribute() {
@@ -19,6 +19,6 @@ class Task extends Model
             return '';
         }
 
-        return self::STATUS[$status]['label'];
+        return self::STATUS[$status]['label']['class'];
     }
 }
