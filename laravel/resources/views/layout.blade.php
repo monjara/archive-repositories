c1fc17d5ad7de555790ea3c8ecda12aaaddef461
monjara <yaginumee@gmail.com>
2020-07-09 10:22:29 +0900

    認証によって画面の切り分け

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -12,6 +12,20 @@
 <header>
   <nav class="my-navbar">
     <a class="my-navbar-brand" href="/">ToDo App</a>
+    <div class="my-navbar-control">
+        @if (Auth::check())
+        <span class="my-navbar-item">ようこそ、{{ Auth::user()->name }}さん</span>
+        |
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <form action="{{  route('logout')}}" method="POST" style="display: none" id="logout-form">
+            @csrf
+        </form>
+        @else
+        <a href="{{ route('login') }}" class="my-navbar-item">ログイン</a>
+        |
+        <a href="{{ route('regiser') }}" class="my-navbar-item">会員登録</a>
+        @endif
+    </div>
   </nav>
 </header>
 <main>
