987f7d68e6721ea7d83bf0ba407430e2c1130537
monjara <yaginumee@gmail.com>
2020-07-09 11:41:12 +0900

    403のポリシークラス

---
+++ laravel/app/Policies/FolderPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Folder;
+use App\User;
+
+
+class FolderPolicy
+{
+
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function view(User $user, Folder $folder) {
+        return $user->id === $folder->user_id;
+    }
+
+}
