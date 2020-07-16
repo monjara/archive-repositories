d12c8a7a41890a2623930bbb7ef63391f9e38af6
monjara <yaginumee@gmail.com>
2020-07-16 15:17:05 +0900

    アクション

---
+++ src/ActionProvider.js
@@ -0,0 +1,9 @@
+class ActionProvider {
+    constructor(createChatBotMessage, setStateFunc) {
+      this.createChatBotMessage = createChatBotMessage;
+      this.setState = setStateFunc;
+    }
+  }
+  
+  export default ActionProvider;
+  
