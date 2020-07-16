cd857034ab125e1b59f9c04c8346c69beed19d8b
monjara <yaginumee@gmail.com>
2020-07-16 15:35:45 +0900

    コメント部分と送信ボタンの色を変更

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  botName: "LearningBot",
+  initialMessages: [createChatBotMessage(`こんにちは、あなたは何を学びたいですか？`)],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#376B7E",
+    },
+    chatButton: {
+      backgroundColor: "#376B7E",
+    },
+  },
 }
 
 export default config;
