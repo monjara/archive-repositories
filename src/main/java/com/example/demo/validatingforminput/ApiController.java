62fc1d1eb0f12984f18949fdd8b527f7833a14d8
monjara <yaginumee@gmail.com>
2020-09-03 01:58:26 +0900

    いったんコミット

--- src/main/java/com/example/demo/validatingforminput/ApiController.java
+++ src/main/java/com/example/demo/validatingforminput/ApiController.java
@@ -1,7 +1,15 @@
 package com.example.demo.validatingforminput;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -10,10 +18,16 @@ public class ApiController {
     AddToStrings ats;
 
     @RequestMapping(method = RequestMethod.POST, value = "/")
-    public PersonForm checkPersonInfo(@RequestBody PersonForm personForm) {
+    public ResponseEntity<Map<String, String>> checkPersonInfo(@Validated @RequestBody PersonForm personForm, BindingResult bindingResult) {
 
-        ats.addAAAAToString(personForm, personForm.getName());
-        return personForm;
+        Map<String, String> message = new HashMap<>();
 
+        if (bindingResult.hasErrors()) {
+            message.put("validation", "error");
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        } else {
+            message.put("validation", "success");
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
     }
 }
--- src/main/java/com/example/demo/validatingforminput/PersonForm.java
+++ src/main/java/com/example/demo/validatingforminput/PersonForm.java
@@ -1,10 +1,19 @@
 package com.example.demo.validatingforminput;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class PersonForm {
 
+    @NotBlank
+    @Size(min=90, max=100)
     private String name;
 
+    @NotNull
+    @Size(max = 3)
     private String age;
 
     public String getAge() {
