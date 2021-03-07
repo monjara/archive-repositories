022483ba29f4a859c04f86e7ca431abc77c8c4ff
monjara <yaginumee@gmail.com>
2021-03-07 15:17:39 +0900

    nameが空文字の場合errorを返す

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name is empty")
+	}
+
 	message := fmt.Sprintf("Hey, %v. Welcome!", name)
-	return message
+	return message, nil
 }
