2b4196ab3cff48b4dfcd29652624844e37075b91
monjara <yaginumee@gmail.com>
2021-03-07 15:45:11 +0900

    mapの利用

--- hello/hello.go
+++ hello/hello.go
@@ -10,9 +10,10 @@ import (
 func main() {
 	log.SetPrefix("greetings:")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
 	if err != nil{
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
