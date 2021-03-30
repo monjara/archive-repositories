f5faea39b1e01854f4ccae2e0a13caca6a34929e
monjara <yaginumee@gmail.com>
2021-03-31 07:36:34 +0900

    stateful wiget作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
-      ),
-    )
-  );
+void main() => runApp(MaterialApp(home: Mypage()));
+
+class Mypage extends StatefulWidget{
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State{
+  int _counter = 0;
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("push button!")),
+      body: Center(child: Text("$_counter")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+      ) ,
+    );
+  }
 }
