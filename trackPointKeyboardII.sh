1507d8269e356f6cf78fbde9fa7b994a8ea49ed9
monjara <yaginumee@gmail.com>
2021-04-27 19:19:41 +0900

    修正

--- trackPointKeyboardII.sh
+++ trackPointKeyboardII.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LTKII=$(xinput list | grep II)
-if [ $LTKII ]; then
-xinput set-button-map "Lenovo TrackPoint Keyboard II Mouse" 1 0 3 4 5 6 7
+/usr/bin/xinput list | /usr/bin/grep II
+if [ $? -eq 0 ]; then
+/usr/bin/xinput set-button-map "Lenovo TrackPoint Keyboard II Mouse" 1 0 3 4 5 6 7
 fi
