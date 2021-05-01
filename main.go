1003506cae49dd46e4f9a1dba76cccc733c81493
monjara <yaginumee@gmail.com>
2021-05-01 15:39:22 +0900

    databaseへの接続、usersテーブルの作成

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
+// User is who access this servise
+type User struct {
+	gorm.Model
+	Name     string    `json:"name"`
+	Age      int       `json:"age"`
+	Birthday time.Time `json:"birthday"`
+}
+
 func main() {
-	r := gin.Default()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World")
-	})
+	db := gormConnect()
 
-	r.Run(":8080")
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+	db.AutoMigrate(&User{})
+
+	defer db.Close()
+	db.LogMode(true)
+}
+
+func gormConnect() *gorm.DB {
+	DBMS := os.Getenv("DBMS")
+	DBCONN := os.Getenv("DATABASE_CONNECTION")
+	db, err := gorm.Open(DBMS, DBCONN)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("connected database: ", &db)
+	return db
 }
