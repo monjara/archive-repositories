006ffaee19fdf0f577cb9d5ac8daca934305f9fe
monjara <yaginumee@gmail.com>
2021-05-04 16:05:26 +0900

    基本的なチャット機能の準備

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+
+	http.HandleFunc("/ws", serveWs)
+}
+
 func main() {
-	fmt.Println("rea-go-chat v0.0.1")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
