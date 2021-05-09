2e270e6209108e752412fbf165679b29fe32f6aa
monjara <yaginumee@gmail.com>
2021-05-09 18:10:31 +0900

    Headerコンポーネント作成

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "test_name";
-  }
   render() {
-    return <h1>Hello! {this.name}!</h1>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
