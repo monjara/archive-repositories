a84bcd16778473edd4d1d259254ccea7806183fb
monjara <yaginumee@gmail.com>
2021-05-09 18:15:43 +0900

    Footerの作成

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Header />
-        <Header />
+        <Footer />
       </div>
     );
   }
