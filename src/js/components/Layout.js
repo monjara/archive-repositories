c5903720ce7990aad7ea8b3af30dd98a1e807c8f
monjara <yaginumee@gmail.com>
2021-05-09 18:45:49 +0900

    stateの変更

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,8 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "test_name" };
+  }
   render() {
-    let components = [<Header />, <Footer />];
-    return <div>{components}</div>;
+    setTimeout(() => {
+      this.setState({ name: "Hello" });
+    }, 1000);
+
+    return (
+      <div>
+        <Header />
+        <div>{this.state.name}</div>
+        <Footer />
+      </div>
+    );
   }
 }
