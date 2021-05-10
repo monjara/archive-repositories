7b6fdc2ef9d9cc4845abc3e79b254e0741f41bcc
monjara <yaginumee@gmail.com>
2021-05-10 19:19:00 +0900

    add react-practice

---
+++ react-practice/README.md
@@ -0,0 +1,11 @@
+# react-practice
+## 学習知見
+### webpack.config.js 書き方
+#### context
+- ベースディレクトリを設定
+#### entry
+- webpackがビルドする際に開始点となるJSファイルを設定
+#### output
+- ビルドファイルを設定
+
+[webpack公式サイト](https://webpack.js.org/concepts/)
---
+++ react-practice/src/js/components/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: "Welcome!" };
+  }
+  changeTitle(title) {
+    this.setState({ title });
+  }
+  render() {
+    return (
+      <div>
+        <Header
+          title={this.state.title}
+          changeTitle={this.changeTitle.bind(this)}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
