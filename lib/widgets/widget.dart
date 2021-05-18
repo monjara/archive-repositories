826f51e8ca2f0e9acf450010a75b70c9eddd32a2
monjara <yaginumee@gmail.com>
2021-05-18 19:56:28 +0900

    タイトル作成

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget AppBarMain(BuildContext context) {
+  return AppBar(
+    centerTitle: true,
+    title: Text("Simcha", style: TextStyle(
+      fontFamily: 'Billabong',
+      fontSize: 38,
+    ),),
+  );
+}
