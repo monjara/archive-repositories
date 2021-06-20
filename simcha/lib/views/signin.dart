4df90ad19b8e39f8291f0be4ea4c94fbf02c4310
monjara <yaginumee@gmail.com>
2021-06-20 13:33:42 +0900

    add simcha

---
+++ simcha/lib/views/signin.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:simcha/widgets/widget.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                cursorColor: Colors.black,
+                decoration: textFieldInputDecoration("E-mail"),
+              ),
+              SizedBox(height: 16,),
+              TextField(
+                cursorColor: Colors.black,
+                decoration: textFieldInputDecoration("password"),
+              ),
+              SizedBox(height: 16,),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                  child: Text("Forgot password?",),
+                ),
+              ),
+              SizedBox(height: 16,),
+              radiusButtonForm(context, "Sign In", Colors.black, Colors.white),
+              SizedBox(height: 16,),
+              radiusButtonForm(context, "Sign In with Google", Colors.white, Colors.black),
+              SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have account? ",style: TextStyle(color: Colors.black),),
+                  Text("Register now",style: TextStyle(color: Colors.black, decoration: TextDecoration.underline),),
+                ],
+              ), 
+              SizedBox(height: 70,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ simcha/lib/widgets/widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    centerTitle: true,
+    backgroundColor: Colors.black,
+    title: Text(
+      "Simcha",
+      style: TextStyle(
+        fontFamily: 'Billabong',
+        fontSize: 38,
+      ),
+    ),
+  );
+}
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+    hintText: hintText,
+    focusedBorder:
+        UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+  );
+}
+
+Container radiusButtonForm(BuildContext context, String text, Color backgroundColor, Color fontColor, [Color borderColor = Colors.black]){
+  return Container(
+    alignment: Alignment.center,
+    width: MediaQuery.of(context).size.width,
+    padding: EdgeInsets.symmetric(vertical: 18),
+    decoration: BoxDecoration(
+      border: Border.all(color: borderColor),
+      gradient: LinearGradient(colors: [
+        backgroundColor,
+        backgroundColor,
+      ]),
+      borderRadius: BorderRadius.circular(30)),
+    child: Text(
+      text,
+      style: TextStyle(color: fontColor, fontSize: 20),
+    ),
+  );
+}
