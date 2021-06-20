6f3ddebdde358483478df531d87fb2c5d4c815d5
monjara <yaginumee@gmail.com>
2021-06-20 15:20:43 +0900

    controller名修正

--- server/route.go
+++ server/route.go
@@ -6,13 +6,13 @@ func GetRoute() {
 	router := gin.Default()
 	folders := router.Group("/folders")
 	{
-		folders.GET("/create", controller.CreateTaskHandler)
-		folders.POST("/create", controller.CreateTaskHandler)
+		folders.GET("/create", controller.CreateFolderController)
+		folders.POST("/create", controller.CreateFolderController)
 
-		folders.GET("/:folderId/tasks", controller.TasksHandler)
-		folders.GET("/:folderId/tasks/create", controller.CreateTaskHandler)
-		folders.POST("/:folderId/tasks/create", controller.CreateTaskHandler)
-		folders.GET("/:folderId/tasks/:taskId/edit", controller.updateTaskHandler)
-		folders.POST("/:folderId/tasks/:taskId/edit", controller.updateTaskHandler)
+		folders.GET("/:folderId/tasks", controller.TasksController)
+		folders.GET("/:folderId/tasks/create", controller.CreateTaskController)
+		folders.POST("/:folderId/tasks/create", controller.CreateTaskController)
+		folders.GET("/:folderId/tasks/:taskId/edit", controller.updateTaskController)
+		folders.POST("/:folderId/tasks/:taskId/edit", controller.updateTaskController)
 	}
 }
