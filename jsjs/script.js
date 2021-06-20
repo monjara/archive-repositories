d1af62bb7c16ca4b0917f6677e0a1b6fae7ec272
monjara <yaginumee@gmail.com>
2021-06-20 13:36:01 +0900

    add jsjs

---
+++ jsjs/script.js
@@ -0,0 +1,10 @@
+function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'クリックされました';
+    document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+}
