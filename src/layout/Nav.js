8ef5c22dd15d2ffcad878a346d6139b7e67bc821
monjara <yaginumee@gmail.com>
2021-07-09 17:42:59 +0900

    NavLinkの利用

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,12 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Nav = () => (
     <nav className='bg-purple-600 text-white p-4 shadow'>
         <ul className='flex space-x-4 justify-end'>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
+            <li>
+                <NavLink exact to='/' activeClassName='border-b-2 border-white'>Home</NavLink>
+            </li>
+            <li>
+                <NavLink exact to='/about' activeClassName='border-b-2 border-white'>About</NavLink>
+            </li>
+            <li>
+                <NavLink exact to='/contact' activeClassName='border-b-2 border-white'>Contact</NavLink>
+            </li>
         </ul>
     </nav>
 );
