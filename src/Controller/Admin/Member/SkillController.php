e7efcc8c67b218b9dcca3c78dd230fbd71104ee8
monjara <yaginumee@gmail.com>
2021-07-24 13:35:33 +0900

    test

--- src/Controller/Admin/Member/SkillController.php
+++ src/Controller/Admin/Member/SkillController.php
@@ -4,18 +4,25 @@
 namespace App\Controller\Admin\Member;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class SkillController
  * @package App\Controller\Admin\Member
  */
-class SkillController
+class SkillController extends AbstractController
 {
     #[Route(path: '/%admin_route%/skill', name: 'admin_skill')]
-    public function index()
+    public function index(Request $request) : Response
     {
-
+        $test = 'こちらてすとです';
+        $contents = $this->renderView('admin/member/skill.twig',[
+            'test' => $test,
+        ]);
+        return new Response($contents);
     }
 
 }
