aff7397350b19660dda5641d26454f8291c73963
monjara <yaginumee@gmail.com>
2021-07-26 00:47:09 +0900

    Introduction to Heuristics Contest - B

--- ans.cpp
+++ ans.cpp
@@ -7,9 +7,24 @@ typedef long long ll;
 //@formatter:on
 
 int main() {
-    double a, b;
-    cin >> a >> b;
-    double c = (a - b) / 3 + b;
-    cout << fixed << setprecision(15) << c << endl;
+    int d;
+    cin >> d;
+    vector<int> c(26);
+    vector<vector<int>> s(d, vector<int>(26));
+    vector<int> t(d);
+    vector<int> last(26, -1);
+    for (int i = 0; i < 26; i++) cin >> c.at(i);
+    for (int i = 0; i < d; i++) for (int j = 0; j < 26; j++) cin >> s.at(i).at(j);
+    for (int i = 0; i < d; i++) cin >> t.at(i);
+    for (int i = 0; i < d; i++) t.at(i)--;
+    ll happy = 0;
+    for (int i = 0; i < d; i++) {
+        happy += s.at(i).at(t.at(i));
+        last.at(t.at(i)) = i;
+        for (int j = 0; j < 26; j++) {
+            happy -= c.at(j) * (i - last.at(j));
+        }
+        cout << happy << endl;
+    }
     return 0;
 }
