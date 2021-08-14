ef4f22d50808f5ad6d56f61b9cfced1380fcf830
monjara <yaginumee@gmail.com>
2021-08-14 20:40:29 +0900

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { FC } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App: FC = () => (
   <div className="App">
