ded3a15c593d87e80eb90b01e6a8b4297a95d43b
monjara <yaginumee@gmail.com>
2021-08-15 14:00:16 +0900

    todo作成

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Todo from "./todo";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Todo />,
   document.getElementById('root')
 );
 
