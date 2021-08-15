318822759014f0af9615e071c597e033d3526e2b
monjara <yaginumee@gmail.com>
2021-08-15 15:18:05 +0900

    useStateの利用

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from "./component/Welcome";
+import Counter from "./component/Counter";
 
 function App() {
     return (
-        <div>
-            <Welcome name='asdf'/>
-            <Welcome name='wefwf'/>
-            <Welcome name='asesdfasdf'/>
-        </div>
+        <Counter />
     );
 }
 
