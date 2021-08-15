1de7bce45c22f8792061e13193127a40810639c0
monjara <yaginumee@gmail.com>
2021-08-15 16:45:23 +0900

    映画検索画面作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,60 @@
-import logo from '../logo.svg';
+import React, {useEffect, useState} from "react";
 import '../App.css';
-import Counter from "./Counter";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
-function App() {
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(MOVIE_API_URL)
+            .then(response => response.json())
+            .then((jsonResponse) => {
+                setMovies(jsonResponse.Search);
+                setLoading(false);
+            });
+    }, []);
+
+    const search = searchValue => {
+        setLoading(true);
+        setErrorMessage(null);
+
+        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+            .then(response => response.json())
+            .then(jsonResponse => {
+                if (jsonResponse.Response === "True") {
+                    setMovies(jsonResponse.Search);
+                    setLoading(false);
+                } else {
+                    setErrorMessage(jsonResponse.Error);
+                    setLoading(false);
+                }
+            });
+    }
+
     return (
-        <Counter />
+        <div className="App">
+            <Header text="HOOKED"/>
+            <Search search={search}/>
+            <p className="App-intro">Sharing a few of our favourite movies</p>
+            <div className="movies">
+                {loading && !errorMessage ? (
+                    <span>loading...</span>
+                ) : errorMessage ? (
+                    <div className="errorMessage">{errorMessage}</div>
+                ) : (
+                    movies.map((movie, index) => (
+                        <Movie key={`${index}-${movie.Title}`} movie={movie}/>
+                    ))
+                )}
+            </div>
+        </div>
     );
-}
+};
 
 export default App;
