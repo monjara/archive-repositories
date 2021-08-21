de39c58de5e5eb7bcd9092aa6af8584506add80a
monjara <yaginumee@gmail.com>
2021-08-21 10:33:07 +0900

    sidebar 修正

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,8 @@
 import { makeStyles, Tab, Tabs } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: "#FBDBD0",
-    display: 'flex',
-    height: window.innerHeight,
-  },
-  tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`,
-  },
-}));
-
 export default function SideBar() {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <Tabs
@@ -30,3 +19,17 @@ export default function SideBar() {
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#FBDBD0",
+    display: 'flex',
+    height: window.innerHeight,
+    width: window.innerWidth / 7.2,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
