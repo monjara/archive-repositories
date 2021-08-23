dc0d7d1801c711ac0049a9e7e8810fbbca6833d8
monjara <yaginumee@gmail.com>
2021-08-23 21:20:43 +0900

    スタイル作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">What needs to be done?</label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button
+          type="submit"
+          className="btn btn__primary btn__lg"
+        >Add</button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button
+          type="button"
+          aria-pressed="false"
+          className="btn toggle-btn"
         >
-          Learn React
-        </a>
-      </header>
+          <span className="visually-hidden">Show </span>
+          <span>All</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button
+          type="button"
+          aria-pressed="false"
+          className="btn toggle-btn"
+        >
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button
+          type="button"
+          aria-pressed="false"
+          className="btn toggle-btn"
+        >
+          <span className="visually-hidden">Show </span>
+          <span>Complete</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        role="list"
+        aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input type="checkbox" id="todo-0" defaultChecked={true}/>
+            <label htmlFor="todo-0" className="todo-label">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input type="checkbox" id="todo-0" defaultChecked={true}/>
+            <label htmlFor="todo-0" className="todo-label">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input type="checkbox" id="todo-0" defaultChecked={true}/>
+            <label htmlFor="todo-0" className="todo-label">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
-  );
+);
 }
 
 export default App;
