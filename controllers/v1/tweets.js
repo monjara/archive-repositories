29b087e59c3e5e89303538664a239d48efcf3265
monjara <yaginumee@gmail.com>
2021-08-30 21:22:12 +0900

    crud準備

--- controllers/v1/tweets.js
+++ controllers/v1/tweets.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'hoge' })
+  res.status(200).json({ message: 'hello' })
+})
+
+router.get('/:id', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
+})
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
 })
 
 module.exports = router
