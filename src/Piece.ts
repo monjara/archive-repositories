026fa17e48c78bbf499dcab0fef97bf446eebe09
monjara <yaginumee@gmail.com>
2021-12-07 21:45:03 +0900

    constructor

--- src/Piece.ts
+++ src/Piece.ts
@@ -1,1 +1,9 @@
-class Piece {}
+type Color = "Black" | "White";
+
+class Piece {
+  protected position: Position;
+
+  constructor(private readonly color: Color, file: File, rank: Rank) {
+    this.position = new Position(file, rank);
+  }
+}
