48c5f0e9b9a1ac08ccabc3373e7677bb0b4a40c2
monjara <yaginumee@gmail.com>
2021-12-07 21:13:10 +0900

    型

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-function add(a: number, b: number): number {
-  return a + b;
+function area(radius: number): number | null {
+  if (radius < 0) {
+    return null;
+  }
+  return Math.PI * radius ** 2;
+}
+
+let r: number = 3;
+let a = area(r);
+if (a != null) {
+  console.info("result: ", a);
 }
