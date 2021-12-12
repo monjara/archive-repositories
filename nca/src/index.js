ddd3b09a178e8c9fc513e184829c1d068e6a14d5
monjara <yaginumee@gmail.com>
2021-12-12 20:53:02 +0900

    io

--- nca/src/index.js
+++ nca/src/index.js
@@ -1,14 +1,27 @@
 const path = require('path')
 const http = require('http')
-
+const socketio = require('socket.io')
 const express = require('express')
+
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectory))
+
+let count = 0
+io.on('connection', (socket) => {
+  console.log('New socket connection')
+  socket.emit('countUpdated', count)
+
+  socket.on('increment', () => {
+    count++
+    io.emit('countUpdated', count)
+  })
+})
 
 server.listen(port, () => {
   console.log(`Server listens on port ${port}`)
