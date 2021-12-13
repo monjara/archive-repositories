4b51d4a00eaccf0221139317c901989b9d40858f
monjara <yaginumee@gmail.com>
2021-12-13 21:50:53 +0900

    fizzbuzz

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-const http = require('http');
+const events = require('events');
 
-const server = http.createServer();
+const createFizzBuzzEventEmitter = (until) => {
+  const eventEmitter = new events.EventEmitter();
+  _emitFizzBuzz(eventEmitter, until);
+  return eventEmitter;
+};
 
-server.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello! world!');
-  res.end();
-});
+const _emitFizzBuzz = async (eventEmitter, until) => {
+  eventEmitter.emit('start');
+  let count = 1;
+  while (count <= until) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    if (count % 15 === 0) {
+      eventEmitter.emit('FizzBuzz', count);
+    } else if (count % 3 === 0) {
+      eventEmitter.emit('Fizz', count);
+    } else if (count % 5 === 0) {
+      eventEmitter.emit('Buzz', count);
+    }
+    count++;
+  }
+  eventEmitter.emit('end', eventEmitter);
+};
 
-server.listen(8000);
+const startListener = () => {
+  console.log('start');
+};
+
+const fizzBuzzListener = (count) => {
+  console.log('FizzBuzz', count);
+};
+
+const fizzListener = (count) => {
+  console.log('Fizz', count);
+};
+
+const buzzListener = (count) => {
+  console.log('Buzz', count);
+};
+
+const endListener = (eventEmitter) => {
+  console.log('end');
+  eventEmitter
+    .off('start', startListener)
+    .off('Fizz', fizzListener)
+    .off('Buzz', buzzListener)
+    .off('FizzBuzz', fizzBuzzListener)
+    .off('end', endListener);
+};
+
+createFizzBuzzEventEmitter(40)
+  .on('start', startListener)
+  .on('Fizz', fizzListener)
+  .on('Buzz', buzzListener)
+  .on('FizzBuzz', fizzBuzzListener)
+  .on('end', endListener);
