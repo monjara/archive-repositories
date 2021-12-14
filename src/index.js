6b85573991ffbd912ac718d9a9578901a525cd28
monjara <yaginumee@gmail.com>
2021-12-14 21:56:09 +0900

    get request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const app = express();
 
-app.use('/api/todos', require('./routes/todos'));
+let todos = [
+  { id: 1, title: 'ネーム', completed: false },
+  { id: 2, title: '下書き', completed: true },
+];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
 
 app.listen(3000);
