b59b183c3d5b94c725900ed4436216d9fc43817a
monjara <yaginumee@gmail.com>
2021-12-15 22:18:56 +0900

    クライアント画面作成

---
+++ client/components/Todo.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+import Link from "next/link";
+import Head from 'next/head';
+import 'isomorphic-fetch';
+
+const pages = {
+  index: { title: 'すべてのToDo', fetchQuery: '' },
+  active: { title: '未完了のToDo', fetchQuery: '?completed=false' },
+  completed: { title: '完了したToDo', fetchQuery: '?completed=true' },
+}
+
+const Todo = (props) => {
+  const { title, page, fetchQuery } = props;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/todos/${fetchQuery}`)
+      .then(async res => {
+        res.ok
+          ? setTodos(await res.json())
+          : alert(res.text())
+      });
+  }, [page])
+
+  const pageLinks = Object.keys(pages).map((page, index) =>
+    <Link
+      href={`/${page === 'index' ? '' : page}`}
+      key={index}
+    >
+      <a style={{ marginRight: 10 }}>{pages[page].title}</a>
+    </Link>
+  );
+
+  return (<>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <h1>{title}</h1>
+    <ul>
+      {todos.map((id, completed) => <li key={id}>
+          <span
+            style={completed ? { textDecoration: 'line-through' } : {}}
+          >
+            {title}
+          </span>
+      </li>)}
+    </ul>
+    <div>{pageLinks}</div>
+  </>);
+}
+
+export default Todo;
