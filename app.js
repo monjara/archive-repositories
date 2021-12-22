822843f83dda2d590acbe2583e83c12aeef7ab22
monjara <yaginumee@gmail.com>
2021-12-22 23:45:18 +0900

    file

--- app.js
+++ app.js
@@ -1,103 +1,27 @@
-'use strict';
-const express = require('express');
+'use strict'
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const dataStorage = require(`./${process.env.npm_lifecycle_event}`)
 
-let todos = [
-  { id: 1, title: 'ネーム', completed: false },
-  { id: 2, title: '下書き', completed: true },
-];
+const app = express()
 
-const app = express();
+app.use(express.json())
 
-app.use(express.json());
-
-app.get('/api/todos', (req, res) => {
-  console.log(req.query);
+app.get('/api/todos', (req, res, next) => {
   if (!req.query.completed) {
-    return res.json(todos);
+    return dataStorage.fetchAll().then(todos => res.json(todos), next)
   }
-  const completed = req.query.completed === 'true';
-  res.json(todos.filter((todo) => todo.completed === completed));
-});
+  const completed = req.query.completed === 'true'
+  dataStorage.fetchByCompleted(completed).then(todos => res.json(todos), next)
+})
 
-let sseSenders = [];
-let sseId = 1;
-app.get('/api/todos/events', (req, res) => {
-  req.socket.setTimeout(0);
-  res.set({
-    'Content-Type': 'text/event-stream',
-  });
-  const send = (id, data) => res.write(`id: ${id}\ndata: ${data}\n\n`);
-  sseSenders.push(send);
-  send(sseId, JSON.stringify(todos));
-  req.on('close', () => {
-    res.end();
-    sseSenders = sseSenders.filter((_send) => _send !== send);
-  });
-});
-
-function onUpdateTodos() {
-  sseId += 1;
-  const data = JSON.stringify(todos);
-  sseSenders.forEach(send => send(sseId, data));
-}
-
-let id = 2;
 app.post('/api/todos', (req, res, next) => {
-  const { title } = req.body;
-  if (typeof title != 'string' || !title) {
-    const err = new Error('title is required');
-    err.statusCode = 400;
-    return next(err);
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    const err = new Error('title is required')
+    err.statusCode(400)
+    return next(err)
   }
-  const todo = { id: id + 1, title: title, completed: false };
-  todos.push(todo);
-  res.status(201).json(todo);
-  onUpdateTodos();
-});
-
-app.use('/api/todos/:id(\\d+)', (req, next) => {
-  const id = Number(req.params.id);
-  const todo = todos.find((todo) => todo.id === id);
-  if (!todo) {
-    const error = new Error('todo not found');
-    error.statusCode = 404;
-    return next(error);
-  }
-  req.todo = todo;
-  next();
-});
-
-app
-  .route('/api/todos/:id(\\d+)/completed')
-  .put((req, res) => {
-    req.todo.completed = true;
-    res.json(req.todo);
-  })
-  .delete((req, res) => {
-    req.todo.completed = false;
-    res.json(req.todo);
-  });
-
-app.delete('/api/todos/:id(\\d+)', (req, res) => {
-  todos = todos.filter((todo) => todo.id !== req.todo.id);
-  res.status(204).end();
-});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(err.statusCode || 500).json({ error: err.message });
-});
-
-app.listen(3000);
-
-const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dev });
-
-nextApp.prepare().then(
-  () => app.get('*', nextApp.getRequestHandler()),
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  }
-);
+  const todo = { id: uuidv4(), title, completed: false }
+  dataStorage.create(todo).then(() => res.status(201).json(todo), next)
+})
---
+++ filesystem/index.js
@@ -0,0 +1,32 @@
+'use strict'
+const { extname } = require('path')
+const { readdir, readFile, writeFile } = require('fs').promises
+
+exports.fetchAll = async () => {
+  const files = (await readdir(__dirname)).filter(file => extname(file) === '.json')
+  return Promise.all(
+    files.map(file => readFile(`${__dirname}/${file}`, 'utf8').then(JSON.parse))
+  )
+}
+
+exports.fetchByCompleted = completed => {
+  exports.fetchAll().then(all =>
+    all.filter(todo => todo.completed === completed)
+  )
+}
+
+exports.create = todo => writeFile(`${__dirname}/${todo.id}.json`, JSON.stringify(todo))
+
+exports.update = async (id, update) => {
+  const fileName = `${__dirname}/${id}.json`
+  return readFile(fileName, "utf-8").then(
+    content => {
+      const todo = {
+        ...JSON.parse(content),
+        ...update
+      }
+      return writeFile(fileName, JSON.stringify(todo)).then(() => todo)
+    },
+    err => err.code === 'ENOENT' ? null : Promise.reject(err)
+  )
+}
